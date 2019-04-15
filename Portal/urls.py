efd2a31ec4bfc0c6b449a48a901f919947941da2
Sanat Kumar <yoursamit15@gmail.com>
2019-04-15 08:43:31 +0530

    updating register models

--- Portal/urls.py
+++ Portal/urls.py
@@ -17,7 +17,6 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url, include
 urlpatterns = [
-
     path('admin/', admin.site.urls),
     url(r'^accounts/', include('registration.backends.default.urls')),
     url('home/', include('register.urls')),
